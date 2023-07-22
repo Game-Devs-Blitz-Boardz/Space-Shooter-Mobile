bc9c202d0d10528e030a5cddaab834a645fc9d20
lillybo <lillian.bojkov@gmail.com>
2023-07-22 04:18:58 +0300

    Adding basic enemy scripts

---
+++ EnemySc/Enemy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] protected float health;
+
+    void Start()
+    {
+        
+    }
+
+    public void takeDamage(float dmg) {
+        health -= dmg;
+        HurtSequence();
+        if (health<=0) {
+            DeathSequence();
+        }
+    }
+
+    public virtual void HurtSequence() {
+        
+    }
+
+    public virtual void DeathSequence() {
+
+    }
+}

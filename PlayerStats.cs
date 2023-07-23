2a9a81bd4746d1f152b37614cc8f97d223f46d20
lillybo <lillian.bojkov@gmail.com>
2023-07-23 15:42:39 +0300

    Adding damage animation to player

--- PlayerStats.cs
+++ PlayerStats.cs
@@ -8,8 +8,11 @@ public class PlayerStats : MonoBehaviour {
     [SerializeField] float maxHealth;
     float health;
 
+    [SerializeField] Animator anim;
     [SerializeField] Image healthFill;
     [SerializeField] protected GameObject explosionPrefab;
+
+    bool canPlayAnim = true;
 
     void Start()
     {
@@ -20,10 +23,20 @@ public class PlayerStats : MonoBehaviour {
     public void PlayerTakeDamage(float damage) {
         health -= damage;
         healthFill.fillAmount = health / maxHealth;
+        if (canPlayAnim) {
+            anim.SetTrigger("Damage");
+            StartCoroutine(AntiSpamAnimation());
+        }
         if (health <= 0) {
             Instantiate(explosionPrefab, transform.position, transform.rotation);
             Destroy(gameObject);
         }
+    }
+
+    IEnumerator AntiSpamAnimation() {
+        canPlayAnim = false;
+        yield return new WaitForSeconds(0.15f);
+        canPlayAnim = true;
     }
 
 }

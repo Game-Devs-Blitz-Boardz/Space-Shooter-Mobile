de87299464b2562cb6e40644ce89f12e38b60de0
lillybo <lillian.bojkov@gmail.com>
2023-07-28 22:57:26 +0300

    GDB-51 adding rewarded ads - basic template / no logic

---
+++ AdsSc/RewardedAds.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class RewardedAds : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener
+{
+    [SerializeField] string _androidAdUnitId = "Rewarded_Android";
+    [SerializeField] string _iOSAdUnitId = "Rewarded_iOS";
+    string _adUnitId = null; // This will remain null for unsupported platforms
+ 
+    void Awake()
+    {   
+        // Get the Ad Unit ID for the current platform:
+#if UNITY_IOS
+        _adUnitId = _iOSAdUnitId;
+#elif UNITY_ANDROID
+        _adUnitId = _androidAdUnitId;
+#endif
+
+        LoadAd();
+
+    }
+
+    public void LoadAd() {
+        if (Advertisement.isInitialized) {
+            Advertisement.Load(_adUnitId, this);
+        }
+    }
+
+    public void ShowAd() {
+        Advertisement.Show(_adUnitId, this);
+    }
+
+    ////////////////////////////////////////////////////////////////
+
+    public void OnUnityAdsAdLoaded(string placementId) {
+
+    }
+
+    public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message) {
+
+    }
+
+    public void OnUnityAdsShowClick(string placementId) {
+        
+    }
+
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState) {
+
+    }
+
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message) {
+
+    }
+
+    public void OnUnityAdsShowStart(string placementId) {
+
+    }
+}

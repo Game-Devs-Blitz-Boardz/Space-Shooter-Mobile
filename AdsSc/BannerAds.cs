22a1a92909e644d72d0b80eebf4c5a0e141d1e8c
lillybo <lillian.bojkov@gmail.com>
2023-07-28 21:52:57 +0300

    GDB-51 adding banner ads

---
+++ AdsSc/BannerAds.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BannerAd : MonoBehaviour
+{
+
+    [SerializeField] int num;
+    [SerializeField] string _androidAdUnitId = "Banner_Android";
+    [SerializeField] string _iOSAdUnitId = "Banner_iOS";
+    string _adUnitId = null;
+    [SerializeField] BannerPosition _bannerPosition = BannerPosition.BOTTOM_CENTER;
+
+    void Start()
+    {
+#if UNITY_IOS
+    _adUnitId = _iOSAdUnitId;
+#elif UNITY_ANDROID
+    _adUnitId = _androidAdUnitId;
+#endif
+
+        LoadBannerAd();
+
+    }
+
+    public void LoadBannerAd() {
+        if (Advertisement.isInitialized) {
+            Advertisement.Banner.SetPosition(_bannerPosition);
+            BannerLoadOptions options = new BannerLoadOptions() {
+                loadCallback = OnBannerLoad, 
+                errorCallback = OnBannerError,
+            };
+            Advertisement.Banner.Load(_adUnitId, options);
+        }
+    }
+
+    void OnBannerLoad () {
+        Advertisement.Banner.Show(_adUnitId);
+    }
+
+    void OnBannerError (string message) {
+        Debug.LogError("Banner Error" + message);
+    }
+}
